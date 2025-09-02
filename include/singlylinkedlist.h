99edcee5e91d7ddd60f732d06220a04418f84ec2
Jihoon <kontang5@icloud.com>
2025-09-03 01:49:11 +0900

    feat: singly linked list

---
+++ include/singlylinkedlist.h
@@ -0,0 +1,38 @@
+#ifndef SINGLYLINKEDLIST_H
+#define SINGLYLINKEDLIST_H
+
+#include <stddef.h>
+#include <stdbool.h>
+
+// Definition
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
+
+typedef struct {
+    Node *head;
+} SinglyLinkedList;
+
+// Memory Management
+void sll_clear(SinglyLinkedList *list);
+
+// Capacity
+size_t sll_size(SinglyLinkedList *list);
+bool sll_is_empty(SinglyLinkedList *list);
+
+// search
+int sll_get(SinglyLinkedList *list, size_t index);
+int sll_find(SinglyLinkedList *list, int value);
+
+// Insertion
+void sll_append(SinglyLinkedList *list, int value);
+void sll_prepend(SinglyLinkedList *list, int value);
+
+// Deletion
+void sll_delete(SinglyLinkedList *list, int value);
+
+// Print
+void sll_print(SinglyLinkedList *list);
+
+#endif
